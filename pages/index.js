b1142a1b2467a4929ba88c359100258f980f5463
Cheese <45247477+ChGGse@users.noreply.github.com>
2021-01-07 10:47:10 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ export default function Index() {
   return (
     <div>
       <Head>
-       <title>avocode</title>
+       <title>codegasm</title>
       </Head>
     <div className="herocont">
       <div className="box">
-      <h1 className="heroheader">avocode</h1>
+      <h1 className="heroheader">codegasm</h1>
       </div>
       <p className="herotext">A coding community for coders and builders.</p>
       <a href="https://guilded.gg/Orangopus"><button className="button">get started</button></a>
